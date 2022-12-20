1ec5385441a95277b4316965d4ba7361121ca431
NathanMuniz <natangabriel.palmeiras@gmail.com>
2022-12-20 20:31:09 -0300

    Application finished again

--- controller/tutorial.controller.js
+++ controller/tutorial.controller.js
@@ -39,16 +39,95 @@ export function findAll(req, res) {
 
 export function findOne(req, res) {
   Tutorial.findById(req.params.id, (err, data) => {
-
     if (err) {
-      res.status(500).send({
-        message:
-          err.message || "Some error find tutorial"
+      if (err.kind = "not_found") {
+        res.status(404).send({
+          message: "Not found especific id"
+        })
+      } else {
+        res.status(500).send({
+          message:
+            err.message || "Some error find tutorial"
 
-      })
+        })
+      }
     } else res.send(data)
   })
 
+}
+
+export function findPublished(req, res) {
+  Tutorial.getAllPublished((err, data) => {
+    if (err) {
+      res.status(500).send(
+        err.message || { message: "error" })
+    } else {
+      res.send(data)
+    }
+  })
+}
+
+
+export function updateTutorial(req, res) {
+  if (!req.params.id || Object.keys(req.body).length === 0) {
+    res.status(400).send({
+      message: "Bad Request, please, insert the id and the new dates"
+    })
+  }
+  const newData = new Tutorial({
+    title: req.body.title,
+    description: req.body.description,
+    published: req.body.published || false
+  })
+
+
+  Tutorial.updateById(req.params.id, newData, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Tutorial with id ${req.params.id}`
+        });
+      } else {
+        req.status(500).send({
+          message: "Error updating TUroial with id" + req.params.id
+        })
+      }
+    } else res.send(data)
+  }
+  )
+}
+
+export function removeAll(req, res) {
+  Tutorial.removeAll((err, data) => {
+    if (err) {
+      res.status(500).send(
+        err.message || { message: "Internal error" })
+    }
+
+    res.send(data)
+
+  })
+}
+
+
+
+
+export function removeById(req, res) {
+
+  Tutorial.remove(req.params.id, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send(
+          err.message || { message: "Not found especific id" }
+        )
+      }
+      res.status(500).send(
+        err.message || { message: "Internal error" }
+      )
+    }
+    res.send(data)
+
+  })
 }
 
 
--- router/router.js
+++ router/router.js
@@ -10,12 +10,20 @@ function route(app) {
   // Create 
   router.post("/", tutorial.create)
 
+  router.get("/published", tutorial.findPublished)
+
   // find all 
   router.get("/", tutorial.findAll)
 
 
   // find one 
   router.get("/:id", tutorial.findOne)
+
+  router.put("/update/:id", tutorial.updateTutorial)
+
+  router.delete("/", tutorial.removeAll)
+
+  router.delete("/:id", tutorial.removeById)
 
   app.use('/api/tutorials', router)
 
